09d884169c6d52ad7ccf4a40212e461a59e0eaa1
Jordan Lee <ljykjh@gmail.com>
2015-01-31 07:58:15 +0900

    multipaxos-1

--- src/multipaxos/java/src/org/apache/nextsql/multipaxos/Replica.java
+++ src/multipaxos/java/src/org/apache/nextsql/multipaxos/Replica.java
@@ -1,5 +1,11 @@
 package org.apache.nextsql.multipaxos;
 
 public class Replica {
-
+  public static enum State {
+    ACTIVE, RECOVERY, CLOSED
+  }
+  
+  // is atomic?
+  static long _slot_num = 1;
+  HashMap<long, >
 }
