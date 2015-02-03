192947e61348661f279529424bdd9d9f2e0fc813
Jordan Lee <ljykjh@gmail.com>
2015-02-04 06:09:02 +0900

    multi-paxos 6%

--- src/multipaxos/java/src/org/apache/nextsql/multipaxos/Replica.java
+++ src/multipaxos/java/src/org/apache/nextsql/multipaxos/Replica.java
@@ -3,21 +3,33 @@ package org.apache.nextsql.multipaxos;
 import java.util.HashMap;
 import org.apache.nextsql.storage.IStorage;
 
-public class Replica {
+public class Replica implements ReplicaService {
+  private boolean _leader = false;
+  private long _blockid;
+  private TNetworkAddress _location;
+  private List<TNetworkAddress> _locations;
+  private long _size = 0;
+  
   // need more think...
   public static enum State {
     ACTIVE,
     RECOVERY,
     CLOSED
   }
+  private State _state = State.ACTIVE;
   
   // must be atomic?
-  private static long _slot_num = 1;
-  private static HashMap<Long, TOperation> _proposals = new HashMap<Long, TOperation>();
-  private static HashMap<Long, TOperation> _decisions = new HashMap<Long, TOperation>();
+  private long _slotNum = 1;
+  private HashMap<Long, TOperation> _proposals = new HashMap<Long, TOperation>();
+  private HashMap<Long, TOperation> _decisions = new HashMap<Long, TOperation>();
   private IStorage _storage = null;
   
-  public Replica(IStorage aStorage) {
+  public Replica(long aBlkId, List<TNetworkAddress> aLocs, boolean aLeader,
+      IStorage aStorage) {
+    this._blockid = aBlkId;
+    this._location = new TNetworkAddress("",0);
+    this._locations = aLocs;
+    this._leader = aLeader;
     this._storage = aStorage;
   }
   
